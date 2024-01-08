96d696d9020cbf5b94e868c572a74d7ef2f959aa
Nacho Fernández <nachofernandez03@gmail.com>
2024-01-08 11:26:59 +0100

    Una vez terminada la implementacion de la clase creamos el patron Singlenton con esta

--- Fichero.js
+++ Fichero.js
@@ -136,297 +136,306 @@ class Coordinate {
     return this.#longitude;
   }
 }
+let RestaurantsManager = function () {
+  let instance;
 
-class RestaurantsManager {
-  #systemName;
-  #categories;
-  #allergens;
-  #dishes;
-  #menus;
-  #restaurants;
+  class RestaurantsManager {
+    #systemName;
+    #categories;
+    #allergens;
+    #dishes;
+    #menus;
+    #restaurants;
 
-  constructor(systemName) {
-    this.#systemName = systemName;
-    this.#categories = [];
-    this.#allergens = [];
-    this.#dishes = [];
-    this.#menus = [];
-    this.#restaurants = [];
-  }
+    constructor(systemName) {
+      this.#systemName = systemName;
+      this.#categories = [];
+      this.#allergens = [];
+      this.#dishes = [];
+      this.#menus = [];
+      this.#restaurants = [];
+    }
 
-  getterDishes() {
-    let iterador = this.#dishes[Symbol.iterator]();
-    return iterador;
-  }
+    getterDishes() {
+      let iterador = this.#dishes[Symbol.iterator]();
+      return iterador;
+    }
 
-  getterCategories() {
-    let iterador = this.#categories[Symbol.iterator]();
-    return iterador;
-  }
-  getterMenus() {
-    let iterador = this.#menus[Symbol.iterator]();
-    return iterador;
-  }
-  getterAllergens() {
-    let iterador = this.#allergens[Symbol.iterator]();
-    return iterador;
-  }
-  getterRestaurants() {
-    let iterator = this.#restaurants[Symbol.iterator]();
-    return iterator;
-  }
-  addCategory(...Categories) {
-    for (const category of Categories) {
-      this.#categories.push(category);
+    getterCategories() {
+      let iterador = this.#categories[Symbol.iterator]();
+      return iterador;
     }
-  }
-  removeCategory(...Categories) {
-    for (const category of Categories) {
-      let index = this.#categories.indexOf(category);
-      if (index != -1) {
-        this.#categories.splice(index, 1);
-      }
+    getterMenus() {
+      let iterador = this.#menus[Symbol.iterator]();
+      return iterador;
     }
-  }
-  addMenu(...Menus) {
-    for (const menu of Menus) {
-      this.#menus.push(menu);
+    getterAllergens() {
+      let iterador = this.#allergens[Symbol.iterator]();
+      return iterador;
     }
-  }
-  removeMenu(...Menus) {
-    for (const menu of Menus) {
-      let index = this.#menus.indexOf(menu);
-      if (index != -1) {
-        this.#menus.splice(index, 1);
-      }
+    getterRestaurants() {
+      let iterator = this.#restaurants[Symbol.iterator]();
+      return iterator;
     }
-  }
-  addAllergen(...Allergens) {
-    for (const allergen of Allergens) {
-      this.#allergens.push(allergen);
-    }
-  }
-  removeAllergen(...Allergens) {
-    for (const allergen of Allergens) {
-      let index = this.#allergens.indexOf(allergen);
-      if (index != -1) {
-        this.#allergens.splice(index, 1);
-      }
-    }
-  }
-  addDish(...Dishes) {
-    for (const dish of Dishes) {
-      this.#dishes.push(dish);
-    }
-  }
-  removeDish(...Dishes) {
-    for (const dish of Dishes) {
-      let index = this.#dishes.indexOf(dish);
-      if (index != -1) {
-        this.#dishes.splice(index, 1);
-      }
-    }
-  }
-  addRestaurant(...Restaurants) {
-    for (const restaurant of Restaurants) {
-      this.#restaurants.push(restaurant);
-    }
-  }
-  removeDish(...Restaurants) {
-    for (const restaurant of Restaurants) {
-      let index = this.#restaurants.indexOf(restaurant);
-      if (index != -1) {
-        this.#restaurants.splice(index, 1);
-      }
-    }
-  }
-  assignCategoryToDish(dish, ...Categories) {
-    // Si el plato no esta en el sistema lo introducimos
-    if (!this.#dishes.includes(dish)) {
-      this.#dishes.push(dish);
-    }
-    // Si las categorias no estan en el sistema las introducimos
-    for (const category of Categories) {
-      if (!this.#categories.includes(category)) {
+    addCategory(...Categories) {
+      for (const category of Categories) {
         this.#categories.push(category);
       }
     }
-    // Ahora asignamos las categorias al plato
-    for (const category of Categories) {
-      if (!dish.Categories.includes(category)) {
-        dish.Categories.push(category);
+    removeCategory(...Categories) {
+      for (const category of Categories) {
+        let index = this.#categories.indexOf(category);
+        if (index != -1) {
+          this.#categories.splice(index, 1);
+        }
       }
     }
-  }
-  deassignCategoryToDish(dish, ...Categories) {
-    for (const category of Categories) {
-      // Conseguimos el indice
-      let index = dish.Categories.indexOf(category);
-      if (!index == -1) {
-        dish.Categories.splice(index, 1);
+    addMenu(...Menus) {
+      for (const menu of Menus) {
+        this.#menus.push(menu);
       }
     }
-  }
-  // Igual pero para las alergias
-  assignAllergenToDish(dish, ...Allergens) {
-    // Si el plato no esta en el sistema lo introducimos
-    if (!this.#dishes.includes(dish)) {
-      this.#dishes.push(dish);
+    removeMenu(...Menus) {
+      for (const menu of Menus) {
+        let index = this.#menus.indexOf(menu);
+        if (index != -1) {
+          this.#menus.splice(index, 1);
+        }
+      }
     }
-    // Si las categorias no estan en el sistema las introducimos
-    for (const allergen of Allergens) {
-      if (!this.#allergens.includes(allergen)) {
+    addAllergen(...Allergens) {
+      for (const allergen of Allergens) {
         this.#allergens.push(allergen);
       }
     }
-    // Ahora asignamos las categorias al plato
-    for (const allergen of Allergens) {
-      if (!dish.Allergens.includes(allergen)) {
-        dish.Allergens.push(allergen);
+    removeAllergen(...Allergens) {
+      for (const allergen of Allergens) {
+        let index = this.#allergens.indexOf(allergen);
+        if (index != -1) {
+          this.#allergens.splice(index, 1);
+        }
       }
     }
-  }
-  deassignAllergenToDish(dish, ...Allergens) {
-    for (const allergen of Allergens) {
-      // Conseguimos el indice
-      let index = dish.Allergens.indexOf(allergen);
-      if (!index == -1) {
-        dish.Allergens.splice(index, 1);
-      }
-    }
-  }
-  assignDishToMenu(menu, ...Dishes) {
-    // Si el menu no esta en el sistema lo introducimos
-    if (!this.#menus.includes(menu)) {
-      this.#menus.push(menu);
-    }
-    for (const dish of Dishes) {
-      if (!this.#dishes.includes(dish)) {
+    addDish(...Dishes) {
+      for (const dish of Dishes) {
         this.#dishes.push(dish);
       }
     }
-    for (const dish of Dishes) {
-      if (!menu.Dishes.includes(dish)) {
-        menu.Dishes.push(dish);
+    removeDish(...Dishes) {
+      for (const dish of Dishes) {
+        let index = this.#dishes.indexOf(dish);
+        if (index != -1) {
+          this.#dishes.splice(index, 1);
+        }
+      }
+    }
+    addRestaurant(...Restaurants) {
+      for (const restaurant of Restaurants) {
+        this.#restaurants.push(restaurant);
+      }
+    }
+    removeRestaurant(...Restaurants) {
+      for (const restaurant of Restaurants) {
+        let index = this.#restaurants.indexOf(restaurant);
+        if (index != -1) {
+          this.#restaurants.splice(index, 1);
+        }
+      }
+    }
+    assignCategoryToDish(dish, ...Categories) {
+      // Si el plato no esta en el sistema lo introducimos
+      if (!this.#dishes.includes(dish)) {
+        this.#dishes.push(dish);
+      }
+      // Si las categorias no estan en el sistema las introducimos
+      for (const category of Categories) {
+        if (!this.#categories.includes(category)) {
+          this.#categories.push(category);
+        }
+      }
+      // Ahora asignamos las categorias al plato
+      for (const category of Categories) {
+        if (!dish.Categories.includes(category)) {
+          dish.Categories.push(category);
+        }
+      }
+    }
+    deassignCategoryToDish(dish, ...Categories) {
+      for (const category of Categories) {
+        // Conseguimos el indice
+        let index = dish.Categories.indexOf(category);
+        if (!index == -1) {
+          dish.Categories.splice(index, 1);
+        }
+      }
+    }
+    // Igual pero para las alergias
+    assignAllergenToDish(dish, ...Allergens) {
+      // Si el plato no esta en el sistema lo introducimos
+      if (!this.#dishes.includes(dish)) {
+        this.#dishes.push(dish);
+      }
+      // Si las categorias no estan en el sistema las introducimos
+      for (const allergen of Allergens) {
+        if (!this.#allergens.includes(allergen)) {
+          this.#allergens.push(allergen);
+        }
+      }
+      // Ahora asignamos las categorias al plato
+      for (const allergen of Allergens) {
+        if (!dish.Allergens.includes(allergen)) {
+          dish.Allergens.push(allergen);
+        }
+      }
+    }
+    deassignAllergenToDish(dish, ...Allergens) {
+      for (const allergen of Allergens) {
+        // Conseguimos el indice
+        let index = dish.Allergens.indexOf(allergen);
+        if (!index == -1) {
+          dish.Allergens.splice(index, 1);
+        }
+      }
+    }
+    assignDishToMenu(menu, ...Dishes) {
+      // Si el menu no esta en el sistema lo introducimos
+      if (!this.#menus.includes(menu)) {
+        this.#menus.push(menu);
+      }
+      for (const dish of Dishes) {
+        if (!this.#dishes.includes(dish)) {
+          this.#dishes.push(dish);
+        }
+      }
+      for (const dish of Dishes) {
+        if (!menu.Dishes.includes(dish)) {
+          menu.Dishes.push(dish);
+        }
+      }
+    }
+    deassignDishToMenu(menu, ...Dishes) {
+      for (const dish of Dishes) {
+        // Conseguimos el indice
+        let index = menu.Dishes.indexOf(dish);
+        if (!index == -1) {
+          menu.Dishes.splice(index, 1);
+        }
+      }
+    }
+    changeDishesPositionsInMenu(menu, dish, dish2) {
+      if (
+        !this.#menus.includes(menu) ||
+        !this.#dishes.includes(dish) ||
+        !this.#dishes.includes(dish2)
+      ) {
+        console.error("Error: Menu or dishes not found in the system.");
+        return;
+      }
+      let index = this.#menus.indexOf(menu); // Sacamos el menu del array
+      let indice1 = this.#menus[index].Dishes.indexOf(dish); // Sacamos de ese menu la posicion del plato1
+      let indice2 = this.#menus[index].Dishes.indexOf(dish2); // Sacamos de ese menu la posicion del plato2
+
+      this.#menus[index].Dishes[indice1] = dish2;
+      this.#menus[index].Dishes[indice2] = dish;
+    }
+
+    getDishesInCategory(category) {
+      let categorias = [];
+
+      for (const dish of this.#dishes) {
+        if (dish.Categories.includes(category)) {
+          categorias.push(dish);
+        }
+      }
+      let iterador = categorias[Symbol.iterator]();
+      return iterador;
+    }
+    getDishesWithAllergen(allergen) {
+      let alergias = [];
+
+      for (const dish of this.#dishes) {
+        if (dish.Allergens.includes(allergen)) {
+          alergias.push(dish);
+        }
+      }
+      let iterador = alergias[Symbol.iterator]();
+      return iterador;
+    }
+    findDishes(plato, callback) {
+      let platos = this.#dishes.filter(
+        (dish) => dish === plato && callback(dish)
+      );
+      return platos[Symbol.iterator]();
+    }
+
+    createDish(name, description, ingredients, image) {
+      let dish = this.#dishes.find((dish) => dish.name == name);
+
+      // Si esta registrado lo devuelve
+      if (dish) {
+        return dish;
+      } else {
+        // Si no lo crea y lo registra
+        dish = new Dish(name, description, ingredients, image);
+        this.#dishes.push(dish);
+      }
+    }
+    createMenu(name, description) {
+      let menu = this.#menus.find((menu) => menu.name == name);
+
+      // Si esta registrado lo devuelve
+      if (menu) {
+        return menu;
+      } else {
+        // Si no lo crea y lo registra
+        menu = new Menu(name, description);
+        this.#menus.push(menu);
+      }
+    }
+    createAllergen(name, description) {
+      let allergen = this.#allergens.find((allergen) => allergen.name == name);
+
+      if (allergen) {
+        return allergen;
+      } else {
+        allergen = new Allergen(name, description);
+        this.#allergens.push(allergen);
+      }
+    }
+    createCategory(name, description) {
+      let category = this.#categories.find((category) => category.name == name);
+
+      // Si esta registrado lo devuelve
+      if (category) {
+        return category;
+      } else {
+        // Si no lo crea y lo registra
+        category = new Category(name, description);
+        this.#categories.push(category);
+      }
+    }
+    createRestaurant(name, description, location) {
+      let restaurant = this.#restaurants.find(
+        (restaurant) => restaurant.name == name
+      );
+
+      // Si esta registrado lo devuelve
+      if (restaurant) {
+        return restaurant;
+      } else {
+        // Si no lo crea y lo registra
+        restaurant = new Restaurant(name, description, location);
+        this.#restaurants.push(restaurant);
       }
     }
   }
-  deassignDishToMenu(menu, ...Dishes) {
-    for (const dish of Dishes) {
-      // Conseguimos el indice
-      let index = menu.Dishes.indexOf(dish);
-      if (!index == -1) {
-        menu.Dishes.splice(index, 1);
-      }
-    }
+  if (!instance) {
+    instance = new RestaurantsManager();
+    return instance;
+  } else {
+    return instance;
   }
-  changeDishesPositionsInMenu(menu, dish, dish2) {
-    if (
-      !this.#menus.includes(menu) ||
-      !this.#dishes.includes(dish) ||
-      !this.#dishes.includes(dish2)
-    ) {
-      console.error("Error: Menu or dishes not found in the system.");
-      return;
-    }
-    let index = this.#menus.indexOf(menu); // Sacamos el menu del array
-    let indice1 = this.#menus[index].Dishes.indexOf(dish); // Sacamos de ese menu la posicion del plato1
-    let indice2 = this.#menus[index].Dishes.indexOf(dish2); // Sacamos de ese menu la posicion del plato2
-
-    this.#menus[index].Dishes[indice1] = dish2;
-    this.#menus[index].Dishes[indice2] = dish;
-  }
-
-  getDishesInCategory(category) {
-    let categorias = [];
-
-    for (const dish of this.#dishes) {
-      if (dish.Categories.includes(category)) {
-        categorias.push(dish);
-      }
-    }
-    let iterador = categorias[Symbol.iterator]();
-    return iterador;
-  }
-  getDishesWithAllergen(allergen) {
-    let alergias = [];
-
-    for (const dish of this.#dishes) {
-      if (dish.Allergens.includes(allergen)) {
-        alergias.push(dish);
-      }
-    }
-    let iterador = alergias[Symbol.iterator]();
-    return iterador;
-  }
-  findDishes(plato, callback) {
-    let platos = this.#dishes.filter(
-      (dish) => dish === plato && callback(dish)
-    );
-    return platos[Symbol.iterator]();
-  }
-
-  createDish(name, description, ingredients, image) {
-    let dish = this.#dishes.find((dish) => dish.name == name);
-
-    // Si esta registrado lo devuelve
-    if (dish) {
-      return dish;
-    } else {
-      // Si no lo crea y lo registra
-      dish = new Dish(name, description, ingredients, image);
-      this.#dishes.push(dish);
-    }
-  }
-  createMenu(name, description) {
-    let menu = this.#menus.find((menu) => menu.name == name);
-
-    // Si esta registrado lo devuelve
-    if (menu) {
-      return menu;
-    } else {
-      // Si no lo crea y lo registra
-      menu = new Menu(name, description);
-      this.#menus.push(menu);
-    }
-  }
-  createAllergen(name, description) {
-    let allergen = this.#allergens.find((allergen) => allergen.name == name);
-
-    if (allergen) {
-      return allergen;
-    } else {
-      allergen = new Allergen(name, description);
-      this.#allergens.push(allergen);
-    }
-  }
-  createCategory(name, description) {
-    let category = this.#categories.find((category) => category.name == name);
-
-    // Si esta registrado lo devuelve
-    if (category) {
-      return category;
-    } else {
-      // Si no lo crea y lo registra
-      category = new Category(name, description);
-      this.#categories.push(category);
-    }
-  }
-  createRestaurant(name, description, location) {
-    let restaurant = this.#restaurants.find(
-      (restaurant) => restaurant.name == name
-    );
-
-    // Si esta registrado lo devuelve
-    if (restaurant) {
-      return restaurant;
-    } else {
-      // Si no lo crea y lo registra
-      restaurant = new Restaurant(name, description, location);
-      this.#restaurants.push(restaurant);
-    }
-  }
-}
+};
 
 let a = new Dish("hola", 2, 3, 4);
 console.log(a.name);
