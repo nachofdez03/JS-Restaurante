e2c86df06534ddbd8e54c192da2ec759b9246a24
Nacho Fernández <nachofernandez03@gmail.com>
2024-01-14 16:33:31 +0100

    terminamos el fichero 2 corrigiendo algunas cosas y metiendo las excepciones

--- Fichero2.js
+++ Fichero2.js
@@ -1,5 +1,14 @@
 // Implementación nueva de la práctica
 
+// Importamos las excepciones mediante un modulos
+import {
+  BaseException,
+  ParameterInvalidException,
+  ExistsException,
+  NoRegistryException,
+} from "./Excepciones.js";
+
+// Clase Dish
 class Dish {
   #name;
   #description;
@@ -29,6 +38,7 @@ class Dish {
   }
 }
 
+// Clase Category
 class Category {
   #name;
   #description;
@@ -48,6 +58,7 @@ class Category {
   }
 }
 
+// Clase allergen
 class Allergen {
   #name;
   #description;
@@ -67,6 +78,7 @@ class Allergen {
   }
 }
 
+// Clase Menu
 class Menu {
   #name;
   #description;
@@ -88,6 +100,7 @@ class Menu {
   }
 }
 
+// Clase Restaurante
 class Restaurant {
   #name;
   #description;
@@ -111,6 +124,7 @@ class Restaurant {
     this.#name = newName;
   }
 }
+// Clase Coordenadas
 class Coordinate {
   #latitude;
   #longitude;
@@ -126,9 +140,11 @@ class Coordinate {
   }
 }
 
+// Singlenton de Restaurant Manager, gracias a esto solo se podra instanciar una vez
 let RestaurantsManager = (function () {
   let instance;
 
+  // Clase RestaurantManager donde estan las colecciones necesarias
   class RestaurantsManager {
     #systemName;
     #categories;
@@ -150,48 +166,73 @@ let RestaurantsManager = (function () {
       return this.#systemName;
     }
 
-    getterDishes() {
-      let iterador = this.#dishes[Symbol.iterator]();
-      return iterador;
+    // Mediante un generador conseguimos los platos que hay en la coleccion
+    *getterDishes() {
+      for (const dishObj of this.#dishes) {
+        yield dishObj.dish;
+      }
     }
-
-    getterCategories() {
-      let iterador = this.#categories[Symbol.iterator]();
-      return iterador;
+    // Otro generador pero aqui al ser los objetos directos y no dentro de objetos literales no hace falta un for of
+    *getterCategories() {
+      yield* this.#categories;
     }
-    getterMenus() {
-      let iterador = this.#menus[Symbol.iterator]();
-      return iterador;
+    // Mediante un generador conseguimos los menus que hay en la coleccion
+    *getterMenus() {
+      for (const menuObj of this.#menus) {
+        yield menuObj.menu;
+      }
     }
-    getterAllergens() {
-      let iterador = this.#allergens[Symbol.iterator]();
-      return iterador;
+    // Otro generador identico a las categorias pero con alergias
+    *getterAllergens() {
+      yield* this.#allergens;
     }
-    getterRestaurants() {
-      let iterator = this.#restaurants[Symbol.iterator]();
-      return iterator;
+    // Otro generador
+    *getterRestaurants() {
+      yield* this.#restaurants;
     }
 
     // Añadimos las categorias
     addCategory(...Categories) {
+      // Recorremos todas las categorias
       for (const category of Categories) {
+        // Comprobamos que no son nulas y son objetos de categorias
+        if (category == null || !(category instanceof Category))
+          throw new ParameterInvalidException("Categoria");
+
+        // Si ya existe
+        if (this.#categories.includes(category))
+          throw new ExistsException("Categoria");
+
+        // La añadimos
         this.#categories.push(category);
       }
     }
 
     // Borramos la cateogoria
     removeCategory(...Categories) {
+      // Las recorremos
       for (const category of Categories) {
+        // Conseguimos el indice
         let index = this.#categories.indexOf(category);
+        // Si existe se borra y si no lanzamos excepcion
         if (index != -1) {
           this.#categories.splice(index, 1);
+        } else {
+          throw new NoRegistryException(category.name);
         }
       }
     }
 
     // Añadimos el Menu
     addMenu(...menus) {
+      // Los recorremos y hacemos las comprobaciones
       for (const menu of menus) {
+        if (menu == null || !(menu instanceof Menu))
+          throw new ParameterInvalidException("Menu");
+
+        if (this.#menus.includes(menu)) throw new ExistsException("menu");
+
+        // Y lo añadimos mediante un objeto literal para añadir la relacion
         this.#menus.push({
           menu,
           dishes: [],
@@ -201,34 +242,59 @@ let RestaurantsManager = (function () {
 
     // Borramos el menu
     removeMenu(...menus) {
+      // Los recorremos
       for (const menu of menus) {
+        // Sacamos el indice
         let index = this.#menus.findIndex((m) => m.menu == menu);
+        // Si esta se borra y si no lanzamos excepcion
         if (index != -1) {
           this.#menus.splice(index, 1);
+        } else {
+          throw new NoRegistryException(menu.name);
         }
       }
     }
 
     // Añadimos Alergia
     addAllergen(...Allergens) {
+      // Recorremos todad las alergias comprobando que son de tipo alergias y no son nulas
       for (const allergen of Allergens) {
+        if (allergen == null || !(allergen instanceof Allergen))
+          throw new ParameterInvalidException("Alergia");
+
+        if (this.#allergens.includes(allergen))
+          throw new ExistsException("Alergia");
+
+        // Las añadimos
         this.#allergens.push(allergen);
       }
     }
 
     // Borramos Alergia
     removeAllergen(...Allergens) {
+      // las recorremos y lanzamos excepciones si no estan bien implementadas
       for (const allergen of Allergens) {
+        // Sacamos el indice y lo comprobamos
         let index = this.#allergens.indexOf(allergen);
+        // Si esta se borra y si no lanzamos excepcion
         if (index != -1) {
           this.#allergens.splice(index, 1);
+        } else {
+          throw new NoRegistryException(allergen.name);
         }
       }
     }
 
     // Añadimos un plato
     addDish(...dishes) {
+      // Recorremos los platos y comprobamos que estan correctamente implementados
       for (const dish of dishes) {
+        if (dish == null || !(dish instanceof Dish))
+          throw new ParameterInvalidException("Dish");
+
+        if (this.#dishes.includes(dish)) throw new ExistsException("Dishes");
+
+        // Los añadimos mediante objetos literales para cumplir la relacion
         this.#dishes.push({
           dish,
           categories: [],
@@ -239,35 +305,55 @@ let RestaurantsManager = (function () {
 
     // Borramos un plato
     removeDish(...dishes) {
+      // Lo recorremos y comprobamos si estan bien implementados
       for (const dish of dishes) {
+        // Comprobamos el indice
         let index = this.#dishes.findIndex((d) => d.dish === dish);
+        // Si esta se borra y si no lanzamos excepcion
         if (index != -1) {
           this.#dishes.splice(index, 1);
+        } else {
+          throw new NoRegistryException(dish.name);
         }
       }
     }
 
     // Añadimos un restaurante
     addRestaurant(...Restaurants) {
+      // Recorremos los restaurantes y comprobamos que estan correctamente implementados
       for (const restaurant of Restaurants) {
+        if (restaurant == null || !(restaurant instanceof Restaurant))
+          throw new ParameterInvalidException("Restaurant");
+
+        if (this.#restaurants.includes(restaurant))
+          throw new ExistsException("restaurant");
+        // Lo añadimos a la coleccion si esta correcto
         this.#restaurants.push(restaurant);
       }
     }
 
     // Borramos restaurante
     removeRestaurant(...Restaurants) {
+      // Los recorremos
       for (const restaurant of Restaurants) {
+        // Comprobamos el indice
         let index = this.#restaurants.indexOf(restaurant);
+        // Si esta se borra y si no excepcion
         if (index != -1) {
           this.#restaurants.splice(index, 1);
+        } else {
+          throw new NoRegistryException(restaurant.name);
         }
       }
     }
 
     // Asignamos la categoria a un plato
     assignCategoryToDish(dish, ...categories) {
-      // Si el plato no esta en el sistema lo introducimos
+      // comprobamos excepcion
+      if (dish == null) throw new ParameterInvalidException("Dish");
       const theDish = this.#dishes.find((d) => d.dish == dish);
+
+      // Si el plato no esta en el sistema lo introducimos
       if (!theDish) {
         this.#dishes.push({
           dish,
@@ -278,6 +364,7 @@ let RestaurantsManager = (function () {
 
       // Si las categorias no estan en el sistema las introducimos
       for (const category of categories) {
+        if (category == null) throw new ParameterInvalidException("Categorias");
         if (!this.#categories.includes(category)) {
           this.#categories.push(category);
         }
@@ -295,17 +382,25 @@ let RestaurantsManager = (function () {
 
     // Desasignamos una categoria a un plato
     deassignCategoryToDish(dish, ...categories) {
+      // Buscamos el objeto literal donde esta ese plato
       const theDish = this.#dishes.find((d) => d.dish == dish);
+      if (dish == null) throw new ParameterInvalidException("Dish");
 
+      // Recorremos categorias
       for (const category of categories) {
+        if (category == null || !theDish.categories.includes(category))
+          throw new ParameterInvalidException("Dish");
+        // Comprobamos indice de la categoria y si esta se desasigna
         let index = theDish.categories.indexOf(category);
-        if (!index == -1) {
+        if (index != -1) {
           theDish.categories.splice(index, 1);
         }
       }
     }
     // Igual pero para las alergias
     assignAllergenToDish(dish, ...allergens) {
+      // Excepciones
+      if (dish == null) throw new ParameterInvalidException("Dish");
       const theDish = this.#dishes.find((d) => d.dish == dish);
 
       // Si no existe el plato, lo añadimos
@@ -317,8 +412,9 @@ let RestaurantsManager = (function () {
         });
       }
 
-      // Si no existe la alergia, la añadimos
+      // Si no existe la alergia, la añadimos comprobandolo
       for (const allergen of allergens) {
+        if (allergen == null) throw new ParameterInvalidException("Allergen");
         if (!this.#allergens.includes(allergen)) {
           this.#allergens.push(allergen);
         }
@@ -334,18 +430,25 @@ let RestaurantsManager = (function () {
 
     // Lo mismo pero con alergias
     deassignAllergenToDish(dish, ...allergens) {
+      // Sacamos el objeto literal del plato
       const theDish = this.#dishes.find((d) => d.dish == dish);
+      if (dish == null || !theDish) throw new ParameterInvalidException("Dish");
 
+      // Las recorremos una por una para comprobar que todo esta ok
       for (const allergen of allergens) {
+        if (allergen == null || !theDish.allergens.includes(allergen))
+          throw new ParameterInvalidException("Allergen");
+        // Indice de la alergia y borramos si eciste
         let index = theDish.allergens.indexOf(allergen);
-        if (!index == -1) {
+        if (index !== -1) {
           theDish.allergens.splice(index, 1);
         }
       }
     }
     assignDishToMenu(menu, ...dishes) {
       // Conseguimos el objeto literal donde esta el menu
-      let theMenu = this.#menus.find((m) => (m.menu = menu));
+      let theMenu = this.#menus.find((m) => m.menu === menu);
+      if (menu == null) throw new ParameterInvalidException("Menu");
 
       // Si el menu no existe
       if (!theMenu) {
@@ -357,8 +460,16 @@ let RestaurantsManager = (function () {
 
       // Si algun plato no existe
       for (const dish of dishes) {
-        if (!this.#dishes.includes(dish)) {
-          this.#dishes.push(dish);
+        if (dish == null) throw new ParameterInvalidException("Dish");
+        let dishObj = this.#dishes.find((d) => d.dish == dish);
+        // Creamos el plato porque es un objeto literal
+        if (!dishObj) {
+          dishObj = {
+            dish,
+            categories: [],
+            allergens: [],
+          };
+          this.#dishes.push(dishObj);
         }
       }
 
@@ -369,25 +480,34 @@ let RestaurantsManager = (function () {
         }
       }
     }
+    // Desasignamos plato del menu
     deassignDishToMenu(menu, ...dishes) {
+      // Objeto literal conseguido
       const theMenu = this.#menus.find((m) => m.menu == menu);
+      if (menu == null || !theMenu) throw new ParameterInvalidException("Menu");
 
+      // Recorremos platos
       for (const dish of dishes) {
+        if (dish == null || !theMenu.dishes.includes(dish))
+          throw new ParameterInvalidException("dish");
+
+        // Indice del plato
         let index = theMenu.dishes.indexOf(dish);
-        if (!index == -1) {
+        if (index !== -1) {
+          // Borramos
           theMenu.dishes.splice(index, 1);
         }
       }
     }
+    // Cambiamos posiciones del menu
     changeDishesPositionsInMenu(menu, dish, dish2) {
+      // Conseguimos Objeto literal del menu
       const theMenu = this.#menus.find((m) => m.menu == menu);
+      if (menu == null || !theMenu) throw new ParameterInvalidException("Menu");
+      if (dish == null || dish2 == null)
+        throw new ParameterInvalidException("dish");
 
-      if (
-        !theMenu ||
-        theMenu.dishes.indexOf(dish) == -1 ||
-        theMenu.dishes.indexOf(dish) == -1
-      )
-        throw new Exception();
+      // Conseguimos el indice de los platos pasados por parametro
 
       let index = theMenu.dishes.indexOf(dish); // Indice Plato 1
       let index2 = theMenu.dishes.indexOf(dish2); // Indice Plato 2
@@ -396,35 +516,52 @@ let RestaurantsManager = (function () {
       theMenu.dishes[index] = dish2;
       theMenu.dishes[index2] = dish;
     }
+    // Conseguimos los platos de alguna categoria
+    *getDishesInCategory(category) {
+      if (category == null || !(category instanceof Category))
+        throw new ParameterInvalidException("Categoria");
+      // metemos en un array las coincidencias para luego devolver el generador de esas coincidencias
+      const dishesInCategory = this.#dishes
+        .filter((dishObj) => dishObj.categories.includes(category))
+        .map((dishObj) => dishObj.dish);
 
-    getDishesInCategory(category) {
-      let categorias = [];
+      // Ordenar los platos (puedes personalizar la lógica de ordenación según tus necesidades)
+      dishesInCategory.sort((dish1, dish2) => {
+        // Aquí puedes comparar propiedades relevantes para la ordenación
+        // Por ejemplo, comparar por nombre
+        return dish1.name.localeCompare(dish2.name);
+      });
 
-      for (const dish of this.#dishes) {
-        let index = dish.categories.indexOf(category);
-        if (index != -1) {
-          categorias.push(category);
-        }
-      }
-      let iterador = categorias[Symbol.iterator]();
-      return iterador;
-    }
-    getDishesWithAllergen(allergen) {
-      let alergias = [];
-
-      for (const dish of this.#dishes) {
-        let index = dish.allergens.indexOf(allergen);
-        if (index != -1) {
-          alergias.push(allergen);
-        }
-      }
-      let iterador = alergias[Symbol.iterator]();
-      return iterador;
+      // Devolver los platos ordenados
+      yield* dishesInCategory;
     }
 
-    findDishes(plato, callback) {
-      let platos = this.#dishes.filter(
-        (dish) => dish === plato && callback(dish)
+    // Igual pero con la alergia
+    *getDishesWithAllergen(allergen) {
+      if (allergen == null || !(allergen instanceof Allergen))
+        throw new ParameterInvalidException("Allergen");
+      // metemos en un array las coincidencias para luego devolver el generador de esas coincidencias
+      let dishesAllergen = this.#dishes
+        .filter((dishObj) => dishObj.allergens.includes(allergen))
+        .map((dishObj) => dishObj.dish);
+
+      dishesAllergen.sort((dish, dish2) => dish.name.localeCompare(dish2.name));
+
+      yield* dishesAllergen;
+    }
+    // Conseguimos el objeto del menu para conseguir los platos
+    *getDishesInMenu(menu) {
+      const menuObj = this.#menus.find((menuObj) => menuObj.menu === menu);
+
+      if (menuObj) {
+        yield* menuObj.dishes;
+      }
+    }
+
+    // Encontramos los platos
+    findDishes(dish, callback) {
+      let dishes = this.#dishes.filter(
+        (dishObj) => dishObj === dish && callback(dishObj)
       );
       return platos[Symbol.iterator]();
     }
@@ -433,8 +570,8 @@ let RestaurantsManager = (function () {
     createDish(name, description, ingredients, image) {
       let index = this.#dishes.findIndex((dish) => dish.name == name);
       // Si esta registrado lo devuelve
-      if (index != -1) {
-        return this.#dishes[index];
+      if (index !== -1) {
+        return this.#dishes[index].dish;
       } else {
         // Si no lo crea y lo registra
         let dish = new Dish(name, description, ingredients, image);
@@ -443,6 +580,7 @@ let RestaurantsManager = (function () {
           categories: [],
           allergens: [],
         });
+        return dish;
       }
     }
     // Creamos Menu
@@ -488,6 +626,7 @@ let RestaurantsManager = (function () {
         this.#categories.push(categorie);
       }
     }
+    // Creamos restaurante
     createRestaurant(name, description, location) {
       let index = this.#restaurants.findIndex(
         (restaurant) => restaurant.name == name
@@ -502,6 +641,7 @@ let RestaurantsManager = (function () {
       }
     }
   }
+  // Esto es lo que devolvemos, una instancia que solo podra ser una
   return {
     getInstance: function (systemName) {
       if (!instance) {
@@ -511,3 +651,14 @@ let RestaurantsManager = (function () {
     },
   };
 })();
+
+// Exportamos las clases
+export {
+  Dish,
+  Category,
+  Allergen,
+  Menu,
+  Restaurant,
+  Coordinate,
+  RestaurantsManager,
+};
